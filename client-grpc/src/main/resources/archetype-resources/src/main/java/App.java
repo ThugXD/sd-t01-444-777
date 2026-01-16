e412624cb06ceb20fbfbf9e4abe634d02d6b5d52
ThugXD <vvalterzandamela@gmail.com>
2026-01-16 21:53:02 +0200

    First Commit

---
+++ client-grpc/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $pt.uevora.sd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
